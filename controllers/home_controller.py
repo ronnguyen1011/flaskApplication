b1124f98ed889710ec71fba52f0b71e93aae99d5
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-05-25 16:15:13 -0700

    5/25 changes

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template
-from models import User
 
 home_controller = Blueprint('home_controller', __name__)
 
 @home_controller.route('/')
 def home():
     # Fetch users from the database
-    users = User.get_all()
 
     # Render the home template and pass the users as context
-    return render_template('home.html', users=users)
+    return render_template('home.html')
+
+@home_controller.route('/personalInfo')
+def personalInfo():
+    return render_template('personalInfo.html')
+
+@home_controller.route('/mailingList')
+def mailingList():
+    return render_template('mailingList.html')
