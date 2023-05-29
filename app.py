9d424f41a3ca662c67df3a07b1999fcdcc618864
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-05-29 07:30:01 -0700

    new version

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask
-from controllers.home_controller import home_controller
+from flask import Flask, redirect, render_template, url_for, session, request
 
 app = Flask(__name__)
-app.secret_key = '123123'  # Set a secret key for session encryption
+app.secret_key = "123123"  # Set a secret key for session encryption
 
-# Register the blueprint for the home-related routes
-app.register_blueprint(home_controller)
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET"])
+def show_form():
+    return render_template("personalInfo.html")
+
+
+@app.route("/", methods=["POST"])
+def process_form():
+    # Access the form data using request.form
+    name = request.form.get("fname")
+    session["data"] = name
+    return redirect(url_for("experience"))
+
+
+@app.route("/experience", methods=["GET"])
+def experience():
+    return render_template("experience.html")
+
+
+@app.route("/experience", methods=["POST"])
+def saveData():
+    name = session.get("data")
+    return redirect(url_for("mailingList"))
+
+
+@app.route("/mailingList", methods=["GET"])
+def mailingList():
+    return render_template("mailingList.html")
+
+
+@app.route("/mailingList", methods=["POST"])
+def mailingListData():
+    return redirect(url_for("summary"))
+
+
+@app.route("/summary", methods=["GET"])
+def summary():
+    data = session.get("data")
+    return render_template("summary.html", data=data)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,46 +1,40 @@
-from flask import Blueprint, render_template,request,session,redirect
+from flask import Blueprint, render_template, request, session, redirect, url_for
 
-home_controller = Blueprint('home_controller', __name__)
-@home_controller.route('/')
-def home():
-    # Fetch users from the database
-
-    # Render the home template and pass the users as context
-    return render_template('home.html')
-
-@home_controller.route('/personalInfo', methods=['GET', 'POST'])
-def personalInfo():
-    if request.method == 'POST':
-        first_name = request.form.get('fname')
-        last_name = request.form.get('lname')
-        email = request.form.get('email')
-        state = request.form.get('state')
-        phone = request.form.get('phone')
-        ml_signup = bool(request.form.get('MLSignUp'))
-
-    # Store the form data in the session
-        session['first_name'] = first_name
-        session['last_name'] = last_name
-        session['email'] = email
-        session['state'] = state
-        session['phone'] = phone
-        session['ml_signup'] = ml_signup
-        return redirect('/mailingList')
-    print(session.get('first_name'))    
-    
-    return render_template('personalInfo.html')
-
-@home_controller.route('/experience')
-def experience():
-    first_name = session.get('first_name')
-    return render_template('experience.html',first_name=first_name)
+# home_controller = Blueprint("home_controller", __name__)
 
 
-@home_controller.route('/mailingList')
-def mailingList():
-    return render_template('mailingList.html')
+# @home_controller.route("/personalInfo")
+# def home():
+#     # Fetch users from the database
 
-@home_controller.route('/summary')
-def summary():
-    fname = session.get('first_name')
-    return render_template('summary.html',fname=fname)
+#     # Render the home template and pass the users as context
+#     return render_template("home.html")
+
+
+# @home_controller.route("/", methods=["GET"])
+# def showForm():
+#     return render_template("personalInfo.html")
+
+
+# @home_controller.route("/", methods=["POST"])
+# def processForm():
+#     first_name = request.form.get("fname")
+#     # Store the form data in the session
+#     session["first_name"] = first_name
+#     return redirect(url_for("home_controller.experience"))
+
+
+# @home_controller.route("/experience", methods=["GET"])
+# def experience():
+#     first_name = session.get("first_name")
+#     return render_template("experience.html", first_name=first_name)
+
+
+# @home_controller.route("/mailingList", methods=["GET"])
+# def mailingList():
+#     return render_template("mailingList.html")
+
+
+# @home_controller.route("/summary", methods=["GET"])
+# def summary():
+#     return render_template("summary.html")
