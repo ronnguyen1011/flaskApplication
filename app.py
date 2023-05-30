d4c643ece5d548dfbff178cf3a09355e408ac433
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-05-30 05:47:22 -0700

    finished

--- app.py
+++ app.py
@@ -12,8 +12,20 @@ def show_form():
 @app.route("/", methods=["POST"])
 def process_form():
     # Access the form data using request.form
-    name = request.form.get("fname")
-    session["data"] = name
+    first_name = request.form.get("fname")
+    last_name = request.form.get("lname")
+    email = request.form.get("email")
+    state = request.form.get("state")
+    phone = request.form.get("phone")
+    ml_signup = bool(request.form.get("MLSignUp"))
+
+    # Store the form data in the session
+    session["first_name"] = first_name
+    session["last_name"] = last_name
+    session["email"] = email
+    session["state"] = state
+    session["phone"] = phone
+    session["ml_signup"] = ml_signup
     return redirect(url_for("experience"))
 
 
@@ -24,7 +36,14 @@ def experience():
 
 @app.route("/experience", methods=["POST"])
 def saveData():
-    name = session.get("data")
+    biography = request.form.get("biography")
+    gitLink = request.form.get("gitLink")
+    year = request.form.get("fav_language")
+    experience = request.form.get("fav_language1")
+    session["biography"] = biography
+    session["gitLink"] = gitLink
+    session["fav_language"] = year
+    session["fav_language1"] = experience
     return redirect(url_for("mailingList"))
 
 
@@ -35,13 +54,41 @@ def mailingList():
 
 @app.route("/mailingList", methods=["POST"])
 def mailingListData():
+    jobs = request.form.getlist("Jobs")
+    industry = request.form.getlist("Industry")
+    session["jobs"] = jobs
+    session["Industry"] = industry
+
     return redirect(url_for("summary"))
 
 
 @app.route("/summary", methods=["GET"])
 def summary():
-    data = session.get("data")
-    return render_template("summary.html", data=data)
+    first_name = session.get("first_name")
+    last_name = session["last_name"]
+    email = session["email"]
+    state = session["state"]
+    phone = session["phone"]
+    gitlink = session["gitLink"]
+    bio = session["biography"]
+    jobs = session["jobs"]
+    industry = session["Industry"]
+    year = session["fav_language"]
+    experience = session["fav_language1"]
+    return render_template(
+        "summary.html",
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        state=state,
+        phone=phone,
+        gitlink=gitlink,
+        bio=bio,
+        jobs=jobs,
+        industry=industry,
+        year=year,
+        experience=experience,
+    )
 
 
 if __name__ == "__main__":
